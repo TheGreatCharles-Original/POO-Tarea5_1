16ecbc9ef7a013327868aacde402a178659203d0
TheGreatCharles-Original <202402263@umh.edu.hn>
2025-08-20 14:48:29 -0600

    Actualizacion

--- Consol/Program.cs
+++ Consol/Program.cs
@@ -1,17 +1,47 @@
-﻿using static System.Console;
+﻿using static System.Console; // es como el std:: del c++ mejor omitirlo
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 
-namespace Calculadora
+namespace Calculator // Una funcion que me permitiria usar Calculator en otro proyecto
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) // Esto crea un argumento con el nombre args, que es de texto
         {
-            WriteLine("Hello World");
+            string name;
+            string lastname;
+
+            int WholeNumber = 5;
+            double DecimalNumber = 2.5; // Para decimales
+            double addition = 0;
+            double subtraction = 0;
+            double division = 0;
+            double multiplication = 0;
+
+            double div1 = 5 / 2, div2 = 5.0 / 2.0, div3 = 5.0 / 2;
+
+            WriteLine($"The division result is {div1},{div2},{div3} ");
+
+            addition = WholeNumber + DecimalNumber;
+            subtraction = WholeNumber - DecimalNumber;
+            division = WholeNumber / DecimalNumber;
+            multiplication = WholeNumber * DecimalNumber;
+
+            name = "September ";
+            lastname = "December 1999";
+
+            WriteLine("Hello World, i am " + name + lastname ); // Concatenacion para agregar una variable se usa +
+            WriteLine($"Hello World,i am {name }{lastname} "); // Interpolacion en lugar de escribir + se usan llaves {
+
+            WriteLine("The sum is " + addition); /* */ //comentario multilenea
+            WriteLine($"The sum is {addition}");
+            WriteLine($"the subtraction is {subtraction}");
+            WriteLine($"The division is {division}");
+            WriteLine($"The multiplication is {multiplication}");
             ReadLine();
         }
     }
--- Consol/obj/Debug/net8.0/Consol.AssemblyInfo.cs
+++ Consol/obj/Debug/net8.0/Consol.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Consol")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42fa320cd361f56b650f591e03d424a7a8b62a39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3c4e6aba63689493c14b446d01aaefb7b1f0577")]
 [assembly: System.Reflection.AssemblyProductAttribute("Consol")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Consol")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
